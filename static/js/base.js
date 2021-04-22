32718b819c3aa34659423ade184d83f0a0581c92
ww-rm <ww-rm@qq.com>
2021-04-22 15:18:57 +0800

    .

---
+++ static/js/base.js
@@ -0,0 +1,17 @@
+const METADATA = "/static/data/metadata.json";
+const CATEGORIES = "/static/data/categories.json";
+const CONTENTS = "./contents.json";
+
+function renderMetadata() {
+
+}
+
+function renderCategories() {
+
+}
+
+function renderContentList() {
+
+}
+
+alert("okkk");
