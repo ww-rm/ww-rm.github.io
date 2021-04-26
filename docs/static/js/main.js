6f6d77531fbe6707b7e8c19262ffb70d4218b76b
ww-rm <ww-rm@qq.com>
2021-04-26 13:48:36 +0800

    add templates

--- docs/static/js/main.js
+++ docs/static/js/main.js
@@ -1,16 +1,4 @@
-// const METADATA = "/static/data/metadata.json";
-// const CATEGORIES = "/static/data/categories.json";
-// const CONTENTS = "./contents.json";
-
-// fetch(METADATA).then(response => {
-//     if (response.ok) {
-//         response.json().then(json => {
-//             console.log(json);
-//         })
-//     } else {
-//         console.log(response.status + ': ' + response.statusText);
-//     };
-// });
 
 PageRenderer.renderMetadata();
 PageRenderer.renderCategories();
+PageRenderer.renderContents();
--- docs/static/js/render.js
+++ docs/static/js/render.js
@@ -10,11 +10,13 @@ const PageRenderer = {
                     const siteMeta = document.querySelector(".site-meta");
 
                     let title = siteMeta.querySelector(".site-title").querySelector("a");
-                    title.innerHTML = json["site-title"];
+                    title.textContent = json["site-title"];
                     title.setAttribute("href", json["site-url"]);
 
                     let subTitle = siteMeta.querySelector(".site-subtitle").querySelector("p");
                     subTitle.innerHTML = json["site-subtitle"];
+
+                    document.title = json["site-title"];
                 })
             } else {
                 console.log(response.status + ': ' + response.statusText);
@@ -31,14 +33,41 @@ const PageRenderer = {
                     const tempNavItem = document.querySelector("#temp-site-nav-item");
                     const siteNavMenu = document.querySelector("#site-nav-menu");
 
+                    siteNavMenu.innerHTML = "";
                     for (let i = 0; i < json.length; i++) {
                         let tmpNode = tempNavItem.content.querySelector("a");
-                        tmpNode.setAttribute("title", json[i]["text"]);
                         tmpNode.setAttribute("href", json[i]["relpath"]);
+                        tmpNode.setAttribute("title", json[i]["text"]);
                         tmpNode = tempNavItem.content.querySelector(".site-nav-menu-item");
                         tmpNode.textContent = json[i]["text"];
 
                         siteNavMenu.appendChild(document.importNode(tempNavItem.content, true));
+                    }
+                })
+            } else {
+                console.log(response.status + ': ' + response.statusText);
+            };
+        }).catch(err => {
+            console.log(err);
+        });
+    },
+
+    renderContents: function () {
+        fetch(PageRenderer.CONTENTS).then(response => {
+            if (response.ok) {
+                response.json().then(json => {
+                    const tempNavItem = document.querySelector("#temp-content-nav-item");
+                    const contentNavMenu = document.querySelector("#content-nav-menu");
+
+                    contentNavMenu.innerHTML = "";
+                    for (let i = 0; i < json.length; i++) {
+                        let tmpNode = tempNavItem.content.querySelector("a");
+                        tmpNode.setAttribute("href", json[i]["relpath"]);
+                        tmpNode.setAttribute("title", json[i]["description"]);
+                        tmpNode = tempNavItem.content.querySelector(".content-nav-menu-item");
+                        tmpNode.textContent = json[i]["description"];
+
+                        contentNavMenu.appendChild(document.importNode(tempNavItem.content, true));
                     }
                 })
             } else {
